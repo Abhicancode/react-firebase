69210d862644e678d695d508eb3a733d2f390973
Abhishek Ojha <ojha439@gmail.com>
2020-08-01 14:31:58 +0545

    Final commit

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAdriff-o3S70SNGZNcAD_ADC31bJxNFLQ",
+  authDomain: "photogallery-ddf6f.firebaseapp.com",
+  databaseURL: "https://photogallery-ddf6f.firebaseio.com",
+  projectId: "photogallery-ddf6f",
+  storageBucket: "photogallery-ddf6f.appspot.com",
+  messagingSenderId: "232456427381",
+  appId: "1:232456427381:web:2e4364e3077c3f7085a53c",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectStorage, projectFirestore, timestamp };
